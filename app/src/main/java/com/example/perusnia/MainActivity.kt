e7533580597e9fb3e565a192f40f94ff9a203f6c
ACH. FASIHUL LISAN <57977183+FasihLisan@users.noreply.github.com>
2022-09-29 22:57:06 +0700

    next revise

--- app/src/main/java/com/example/perusnia/MainActivity.kt
+++ app/src/main/java/com/example/perusnia/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.perusnia
 
+import FavoriteFragment
+import HomeFragment
+import NoteFragment
+import ProfileFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.perusnia.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment=HomeFragment()
+        val favoriteFragment=FavoriteFragment()
+        val noteFragment=NoteFragment()
+        val profileFragment=ProfileFragment()
+
+
+        setCurrentFragment(homeFragment)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(homeFragment)
+                R.id.favorite->setCurrentFragment(favoriteFragment)
+                R.id.note->setCurrentFragment(noteFragment)
+                R.id.profile->setCurrentFragment(profileFragment)
+
+
+            }
+            true
+        }
+
     }
+
+    private fun setCurrentFragment(fragment:Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
 }
