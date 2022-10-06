662ba6933c0dc0504991d831e1213fde82eeaa66
ACH. FASIHUL LISAN <57977183+FasihLisan@users.noreply.github.com>
2022-10-06 08:39:16 +0700

    next revise

--- app/src/main/java/com/example/perusnia/MainActivity.kt
+++ app/src/main/java/com/example/perusnia/MainActivity.kt
@@ -1,9 +1,5 @@
 package com.example.perusnia
 
-import FavoriteFragment
-import HomeFragment
-import NoteFragment
-import ProfileFragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
